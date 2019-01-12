97bc89f244486b289b70d50aebb7cb538e03310a
jorgevilasboas <jorge.sergipe@gmail.com>
2019-01-12 18:37:27 -0300

    Adds Card and CardSection

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+    const { title, artist, thumbnail_image, image, url } = album;
     return (
-        <Text>{props.album.title}</Text>
-    );    
+        <Card>
+            <CardSection>
+                <View style={styles.thumbnailContainerStyle}>
+                    <Image
+                        style={styles.thumbnailStyle}
+                        source={{ uri: thumbnail_image }}
+                    />
+                </View>
+                <View style={styles.headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+        </Card>
+    );
 }
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
+};
 
 export default AlbumDetail;
