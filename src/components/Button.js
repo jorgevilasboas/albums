fbd83c69c4182119e168399042d1ff2fb910730e
jorgevilasboas <jorge.sergipe@gmail.com>
2019-01-15 22:12:08 -0300

    Adds TouchableOpacity Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { TouchableOpacity, Text } from 'react-native';
 
 const Button = () => {
     return (
-        <Text>Click me!!!</Text>
+        <TouchableOpacity style={styles.buttonStyle}>
+            <Text style={styles.textStyle}>Click me!!!</Text>
+        </TouchableOpacity>
     );
+}
+
+const styles = {
+    textStyle: {
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007aff',
+        marginLeft: 5,
+        marginRight: 5,
+    }
 }
 
 export default Button;
